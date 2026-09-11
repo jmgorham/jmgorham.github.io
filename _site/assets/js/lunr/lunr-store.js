2969923e20bde4a283504090d8c48fa25efd9c16
Jenna Gorham <jenna.m.gorham@gmail.com>
2026-09-11 13:50:47 -0600

    Configure base site layout including homepage and navigation structure

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Post: Modified Date",
-        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
-        "tags": ["Post Formats","readability","standard"],
-        "url": "/blog/post-modified/",
-        "teaser": null
-      }]
+        "title": "Foo Bar Identity",
+        "excerpt":"Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section...","categories": [],
+        "tags": [],
+        "url": "/portfolio/foo-bar/",
+        "teaser": "/portfolio/assets/images/foo-bar-identity-th.jpg"
+      },]
